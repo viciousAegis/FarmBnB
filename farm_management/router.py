c8daa469eaa43a0a316519086fb7466400c3e12c
sriya <sriya.venepally@students.iiit.ac.in>
2024-04-16 23:34:42 +0530

    testing farm_management

--- farm_management/router.py
+++ farm_management/router.py
@@ -1,1 +1,10 @@
-print("hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/farm/list", methods=["GET"])
+def list_farms():
+    return "List of farms"
+
+if __name__ == "__main__":
+    app.run()
