b6ffe68b0056239f721f1c997b4b46f421b36387
Akshit Sinha <akshitsinha3@gmail.com>
2024-04-16 23:52:09 +0530

    boilerplate setup

--- farm_management/router.py
+++ farm_management/router.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/farm/list", methods=["GET"])
+'''
+this is the health check endpoint for the farm server.
+this is used to check if the farm server is running.
+'''
+@app.route('/health')
+def health_check():
+    return 'OK'
+
+@app.route("/farm/list", methods=["POST"])
 def list_farms():
-    return "List of farms"
+    print("List of farms")
+    print(request.json)
+    return {"message": "List of farms"}
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5001) # port for the farm server (5001)
---
+++ main_server.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+import subprocess
+import requests
+
+app = Flask(__name__)
+
+@app.route('/farm/<path:subpath>', methods=['POST'])
+def run_farm(subpath):
+    farm_port = 5001
+    
+    farm_process = subprocess.Popen(['python3', 'farm_management/router.py'])
+    
+    # Check if the server is running by making a request to the health check endpoint
+    farm_health_url = f'http://127.0.0.1:{farm_port}/health'
+    while True:
+        try:
+            response = requests.get(farm_health_url)
+            print(response.text)
+            if response.text == 'OK':
+                break  # Server is up and running
+        except requests.ConnectionError:
+            pass  # Server is not yet available, try 
+        
+    farm_url = f'http://localhost:{farm_port}/farm/{subpath}'
+    farm_response = requests.post(farm_url, json=request.json)
+    
+    # terminate the farm server
+    farm_process.terminate()
+    
+    return farm_response.json()
+
+if __name__ == '__main__':
+    app.run(port=5000) # port for the main server
