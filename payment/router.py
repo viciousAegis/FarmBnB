07a33e8865fece0dd72876d0cb1ccdf3b76c7782
muktachanda <muktachanda03@gmail.com>
2024-04-23 18:31:35 +0530

    added payment subsystem

--- payment/router.py
+++ payment/router.py
@@ -1,7 +1,11 @@
 import os
-from flask import Flask, jsonify
+import random
+import string
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from payment_strategy import UpiPaymentStrategy, CardPaymentStrategy
+
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path)
@@ -21,10 +25,42 @@ except Exception as e:
     print(f"Failed to connect to MongoDB: {e}")
 
 
+@app.route("/pay/book", methods=["POST"])
+def process_payment_and_book():
+    user_id = request.json.get("user_id")
+    total_price = request.json.get("total_price")
+    payment_info = request.json.get("payment_info")
+    
+    payment_method = request.json.get("payment_method")
+    if payment_method == "upi":
+        payment_strategy = UpiPaymentStrategy()
+    elif payment_method == "card":
+        payment_strategy = CardPaymentStrategy()
+    else:
+        return jsonify({"status": "error", "message": "Invalid payment method"})
+    payment_result = payment_strategy.process_payment(user_id, total_price, payment_info)
+    if payment_result["status"] == "success":
+        user_collection.update_one({"user_id": user_id}, {"$inc": {"wallet_balance": -total_price}})
+        return jsonify(payment_result)
+    else:
+        return jsonify({"status": "error", "message": f"Payment using {payment_method} failed"})
 
-@app.route("/pay/upi", methods=["GET"])
-def upi_payment():
-    return jsonify({"message": "UPI payment successful"})
+
+@app.route('/pay/wallet', methods=['POST'])
+def pay_using_wallet():
+    user_id = request.json.get("user_id")
+    total_price = int(request.json.get("total_price"))
+    user = user_collection.find_one({"user_id": user_id})
+    if user is None:
+        return jsonify({"status": "error", "message": "User not found"})
+    if user["wallet_balance"] < total_price:
+        return jsonify({"status": "error", "message": "Insufficient balance"})
+    user_collection.update_one({"user_id": user_id}, {"$inc": {"wallet_balance": -total_price}})
+    return jsonify({"status": "success", "message": "Payment successful using wallet"})
+
+@app.route('/health')
+def health_check():
+    return "OK"
 
 if __name__ == "__main__":
     app.run(debug=False, port=5002)
