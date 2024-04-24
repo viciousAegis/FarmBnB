39f55c6a2c08faa03799dfc852db098d07a5ae9d
sriya <sriya.venepally@students.iiit.ac.in>
2024-04-24 23:53:52 +0530

    added date availability checking

--- farm_management/router.py
+++ farm_management/router.py
@@ -7,7 +7,7 @@ from farm import Farm
 from farmDao import FarmDao
 from userFarm import UserFarm
 from userFarmDao import UserFarmDao
-import datetime
+from datetime import datetime
 from bson.objectid import ObjectId
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
@@ -81,13 +81,32 @@ def list_userfarms():
 
 @app.route("/farm/check_availability", methods=["GET"])
 def check_availability():
-    start_date = request.args.get("start_date")
-    end_date = request.args.get("end_date")
+    start_date_str = request.json["start_date"]
+    end_date_str = request.json["end_date"]
+    farm_id = request.json["farm_id"]
+
+    start_date = datetime.strptime(start_date_str, "%Y-%m-%d").date()
+    end_date = datetime.strptime(end_date_str, "%Y-%m-%d").date()
+
 
     if not start_date or not end_date:
         return jsonify({"error": "start_date and end_date are required"}), 400
     
+    if not farm_id:
+        return jsonify({"error": "farm_id is required"}), 400
+    
+    userFarmDao = UserFarmDao(db)
+    userfarms = userFarmDao.get_userfarm_by_farmid(farm_id).get_json()
+
+    for userfarm in userfarms:
+        booked_start_date = datetime.strptime(userfarm["start_date"], "%Y-%m-%d").date()
+        booked_end_date = datetime.strptime(userfarm["end_date"], "%Y-%m-%d").date()
+
+        if (start_date < booked_start_date and end_date >= booked_start_date) or (start_date >= booked_start_date and start_date <= booked_end_date):
+            return jsonify({"available": False})
+        
+    return jsonify({"available": True})
     
 
 if __name__ == "__main__":
-    app.run(port=5003) # port for the farm server (5001)
+    app.run(port=5001) # port for the farm server (5001)
